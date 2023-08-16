2dc40f166be0f76e9bc667f0922dd93d3376985f
Maulanaardan <maulanawardana01gmail.com>
2023-08-16 12:10:39 +0700

    resposive

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,36 @@
 /*? no js js needed from me */
 function hiden(e) {
-  const target = $(e.target)
+  const target = $(e.target);
 
-  if($(target).hasClass("active")) {
-    $(target).html("See More").removeClass("active")
+  if ($(target).hasClass("active")) {
+    $(target).html("See More").removeClass("active");
+  } else {
+    $(target).html("Less More").addClass("active");
   }
-  else{
-    $(target).html("Less More").addClass("active")
+
+  const parents = $(e.target).parents(".about-exp");
+  const detail = $(parents).children(".about-exp-detail");
+
+  $(detail).slideToggle();
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const email = $("#inputEmail");
+  const subject = $("#inputSubject");
+  const message = $("#inputMessage");
+
+  if (!$(email).val()) {
+    alert("Email is required");
+  } else if (!$(subject).val()) {
+    alert("Subject is required");
+  } else if (!$(message).val()) {
+    alert("Message is required");
+  } else {
+    alert("nice");
+    $(email).val("");
+    $(subject).val("");
+    $(message).val("");
   }
-
-  const parents =  $(e.target).parents(".about-exp")
-  const detail = $(parents).children(".about-exp-detail")
-
-  $(detail).slideToggle()
-
-  
 }
